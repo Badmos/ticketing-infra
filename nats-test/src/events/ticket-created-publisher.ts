60c2d293b6dd2eddfd830d252cf3f9d177271a95
Yusuf <badmosy@gmail.com>
2020-08-15 06:21:20 +0100

    split k8s manifest files to dev and prod, remove nats test

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from './base-publisher';
-import { TicketCreatedEvent } from './ticket-created-event';
-import { Subjects } from './subject';
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  readonly subject = Subjects.TicketCreated;
-}
