276190c3f6e73aafccd84f34a70b1c079ead98ba
Yusuf <badmosy@gmail.com>
2020-08-15 07:46:02 +0100

    rebuilt docker image of all services

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -8,6 +8,7 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
+	console.log("Starting server >>>>>");
 	//check if environment variable is defined in pod
 	if (!process.env.JWT_KEY) {
 		throw new Error("JWT key has not been set as an environment variable");
